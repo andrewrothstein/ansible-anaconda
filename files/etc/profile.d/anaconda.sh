dbc76dd04da198984c4bfe47a196894e1c2e9701
Vishal Shah <vishah@blackrock.com>
2016-09-29 17:38:46 -0400

    Fix - added missing export

--- files/etc/profile.d/anaconda.sh
+++ files/etc/profile.d/anaconda.sh
@@ -1,5 +1,4 @@
-ANACONDA_DIR=/opt/anaconda
-PATH=${ANACONDA_DIR}/bin:${PATH}
-PYTHON=${ANACONDA_DIR}/bin/python
-LD_LIBRARY_PATH=${ANACONDA_DIR}/lib:${LD_LIBRARY_PATH}
-
+export ANACONDA_DIR=/opt/anaconda
+export PATH=${ANACONDA_DIR}/bin:${PATH}
+export PYTHON=${ANACONDA_DIR}/bin/python
+export LD_LIBRARY_PATH=${ANACONDA_DIR}/lib:${LD_LIBRARY_PATH}
